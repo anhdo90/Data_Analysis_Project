b86524c47e03927c0d2903764d8b1992236080c8
Anh Do <70506992+anhdo90@users.noreply.github.com>
2024-03-13 02:44:06 -0400

    Add files via upload

---
+++ Sale Report/Cleansing & Transformation/FACT_InternetSale.sql
@@ -0,0 +1,32 @@
+--Cleanse FACT_InternetSales Table --
+SELECT [ProductKey]
+      ,[OrderDateKey]
+      ,[DueDateKey]
+      ,[ShipDateKey]
+      ,[CustomerKey]
+      --,[PromotionKey]
+      --,[CurrencyKey]
+      --,[SalesTerritoryKey]
+      ,[SalesOrderNumber]
+      --,[SalesOrderLineNumber]
+      --,[RevisionNumber]
+      --,[OrderQuantity]
+      --,[UnitPrice]
+      --,[ExtendedAmount]
+      --,[UnitPriceDiscountPct]
+      --,[DiscountAmount]
+      --,[ProductStandardCost]
+      --,[TotalProductCost]
+      ,[SalesAmount]
+      --,[TaxAmt]
+      --,[Freight]
+      --,[CarrierTrackingNumber]
+      --,[CustomerPONumber]
+      --,[OrderDate]
+      --,[DueDate]
+      --,[ShipDate]
+  FROM [AdventureWorksDW2019].[dbo].[FactInternetSales]
+  WHERE 
+	LEFT(OrderDateKey,4) >= YEAR(GETDATE()) - 5 --Ensure we always only bring two years of date from extraction
+  ORDER By
+	OrderDateKey
